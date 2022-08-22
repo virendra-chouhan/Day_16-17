457095b88761808b49f29f49f4043ae15a381f4c
virendra <virendrachouhan1494@gmail.com>
2022-08-22 21:01:58 +0530

    Mergesort

---
+++ AlgorithmProblems/MergeSort.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+      class MergeSort
+    {
+        public static void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            int[] left = new int[n1];
+            int[] right = new int[n2];
+            int i, j;
+            for (i = 0; i < n1; ++i)
+            {
+                left[i] = arr[l + i];
+            }
+            for (j = 0; j < n2; ++j)
+            {
+                right[j] = arr[m + 1 + j];
+            }
+
+            i = 0;
+            j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+        ///Sorting method
+        public static void Sort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+
+                int m = (l + r) / 2;
+                Sort(arr, l, m);
+                Sort(arr, m + 1, r);
+                Merge(arr, l, m, r);
+            }
+        }
+        public static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(" "+arr[i]);
+            Console.WriteLine();
+        }
+    }
+}
--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -16,6 +16,7 @@ namespace AlgorithmProblems
             Console.WriteLine("2.Binary Search the word from wordlist");
             Console.WriteLine("3.Insertion Sort");
             Console.WriteLine("4.Bubble Sort");
+            Console.WriteLine("5.Merge sort");
             Console.WriteLine("Enter the option");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
@@ -49,6 +50,13 @@ namespace AlgorithmProblems
                     int[] arr1 = { 10, 2, 14, 3, 1, 5, 9 };
                     BubbleSort.BubbleSortInArray(arr1);
                     BubbleSort.PrintArray(arr1);
+                    break;
+
+                    ///For UseCase5
+                case 5:
+                    int[] arr2 = { 32, 12, 9, 65, 24, 5 };
+                    MergeSort.Sort(arr2, 0, arr2.Length - 1);
+                    MergeSort.PrintArray(arr2);
                     break;
 
                 default:

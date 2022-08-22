6b834ce4da691bda9060a557ec2f68217310eb8f
virendra <virendrachouhan1494@gmail.com>
2022-08-22 20:44:44 +0530

    Permutations

---
+++ AlgorithmProblems/Permutations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    class Permutations
+      {
+
+        public static void Permutate( string str, int start, int end)
+        {
+            if (start == end)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    str = (Swap(str, start, i));
+                    Permutate(str, start + 1, end);
+                    str = Swap(str, start, i);
+                }
+            }
+
+        }
+        public static string Swap(string word, int start, int end)
+        {
+            char temp;
+            char[] charArray = word.ToCharArray();
+            temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+            string s = new string (charArray);
+            return s;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
+
---
+++ AlgorithmProblems/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+      class Program
+      {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("1.Permutation Using Recursion");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                ///For UseCase1
+                case 1:
+                    Console.WriteLine("Enter String to find Permutation ");
+                    string str = Console.ReadLine();
+                    int n = str.Length;
+                    Permutations.Permutate(str, 0, n - 1);
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the valid option!");
+                    break;
+            }
+            Console.WriteLine("\nDo you want to continue");
+            Console.ReadLine();
+        }
+      }
+    
+}
